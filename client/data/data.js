4deaeeb0444431dc26118f552ec0282fa8ca9526
Patrick Ford <patrickford@msn.com>
2014-01-25 00:14:27 -0800

    New songs added.

--- client/data/data.js
+++ client/data/data.js
@@ -16,9 +16,18 @@ var songData = [
     artist: "Aaliyah",
   },
   {
-    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3",
+    url: "file:///Users/patrickford/downloads/JJ Grey & Mofro - 99 Shades Of Crazy.mp3",
     title: "If Your Girl Only Knew",
+    artist: "JJ Grey & Mofro",
+  },  {
+    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3",
+    title: "99 Shades of Crazy",
     artist: "Aaliyah",
+  },
+  {
+    url: "https://ia700501.us.archive.org/19/items/ChuckBerry-01-20/ChuckBerry-JohnnyBGood.mp3",
+    title: "Johnny B Goode",
+    artist: "Chuck Berry",
   },
   {
     url: "http://communityguitar.com/students/Songs/sound_clips/HesitationBlues_HotTuna.mp3",
